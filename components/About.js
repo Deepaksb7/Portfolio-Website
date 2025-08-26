ffbe24a8a73fc5b2bf73f8e9281f6f08c9959128
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-26 12:29:55 +0530

    Header Completed

---
+++ components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h4>
+        Introductin
+      </h4>
+      <h2>
+        About me
+      </h2>
+    </div>
+  )
+}
+
+export default About
