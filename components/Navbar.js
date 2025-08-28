10b128fd5f152ce6ef2c0337154e342334e466d4
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-29 01:42:07 +0530

    hover issue fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -58,7 +58,7 @@ const Navbar = ({isDarkMode,setisDarkMode}) => {
 
         </div>
 
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bg-rose-50 bottom-0 w-64 z-50 transition duration-500 dark:bg-darkHover dark:text-white'>
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bg-rose-50 bottom-0 w-64 z-50 transition duration-500 dark:bg-darkhover dark:text-white'>
 
             <div className='absolute top-6 right-6' onClick={closeMenu}>
               <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-4 cursor-pointer'/>
