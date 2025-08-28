84dad4cb043511a2ac2c97982323e9964ba69379
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-29 01:08:48 +0530

    solved the ' issue

--- components/Contact.js
+++ components/Contact.js
@@ -54,7 +54,7 @@ const Contact = () => {
     whileInView={{opacity:1}}
     transition={{duration:0.5,delay:0.7}}
             className='text-center max-w-2xl mx-auto mt-5 mb-12 py-5 font-Ovo'>
-                I'd love to hear from you! If you have any questions, comments, or feedback, please use the form bellow.
+                I would love to hear from you! If you have any questions, comments, or feedback, please use the form bellow.
             </motion.p>
 
             <motion.form
